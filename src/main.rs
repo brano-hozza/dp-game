923c9a8dffd4da5012952ed6ff07a0584669691a
Branislav Hozza <brankohozza@gmail.com>
2024-09-23 19:57:54 +0200

    feat: prepare map gen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use bevy_fast_tilemap::plugin::FastTileMapPlugin;
 use dp_game::DpPlugin;
 
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Fast Tilemap example"),
+                    resolution: (1820., 920.).into(),
+                    // disable vsync so we can see the raw FPS speed
+                    present_mode: PresentMode::Immediate,
+                    ..default()
+                }),
+                ..default()
+            }),
+            FastTileMapPlugin::default(),
+        ))
         .add_plugins(DpPlugin)
         .run();
 }
