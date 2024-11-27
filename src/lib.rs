47f0ff48cb63da909bca979c307cce7c316725fc
Branislav Hozza <brankohozza@gmail.com>
2024-11-27 23:53:53 +0100

    feat: working txs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 use std::{str::FromStr, sync::Arc};
 
 use alloy::{
-    network::{EthereumWallet, TransactionBuilder},
+    network::{Ethereum, EthereumWallet},
     primitives::{address, Address, Uint},
-    providers::{self, Provider, ProviderBuilder, RootProvider},
-    rpc::types::Filter,
+    providers::{
+        fillers::{
+            BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller,
+            WalletFiller,
+        },
+        Identity, Provider, ProviderBuilder, RootProvider,
+    },
     signers::local::PrivateKeySigner,
     sol_types::sol,
     transports::http::{Client, Http},
 };
-use alloy_sol_types::SolEvent;
 use bevy::{prelude::*, tasks::futures_lite::StreamExt};
 use bevy_tokio_tasks::TokioTasksRuntime;
 use common::systems::print_position_system;
@@ -21,15 +25,12 @@ use score::{
 };
 use GoldContract::GoldContractInstance;
 use SwordContract::SwordContractInstance;
-// alloy - alternative to web3
 
 pub mod common;
 pub mod map;
 pub mod player;
 pub mod score;
 pub mod ui;
-
-const MAX_TESTNET_GAS_PRICE: u128 = 20_000_000_000;
 
 const ALICE_ADDRESS: &str = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const ALICE_PK: &str = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
@@ -44,10 +45,23 @@ sol!(
 );
 
 type HttpClient = Http<Client>;
-type ContractProvider = Arc<RootProvider<Http<Client>>>;
+type ContractProvider = Arc<
+    FillProvider<
+        JoinFill<
+            JoinFill<
+                Identity,
+                JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
+            >,
+            WalletFiller<EthereumWallet>,
+        >,
+        RootProvider<Http<Client>>,
+        Http<Client>,
+        Ethereum,
+    >,
+>;
 
 #[derive(Resource)]
-pub struct EthProviderResource(Arc<ContractProvider>);
+pub struct EthProviderResource(ContractProvider);
 
 #[derive(Resource)]
 pub struct GoldContractResource(Arc<GoldContractInstance<HttpClient, ContractProvider>>);
@@ -87,119 +101,142 @@ fn try_mint_sword(
     provider: Res<EthProviderResource>,
     sword_contract_res: Res<SwordContractResource>,
 ) {
-    let provider = provider.0.clone();
     let sword_contract = sword_contract_res.0.clone();
+    let provider = provider.0.clone();
     runtime.spawn_background_task(|_ctx| {
         // Move everything into the async block, ensuring all variables are owned.
         async move {
             let account_alice = Address::from_str(ALICE_ADDRESS).unwrap();
-            let signer: PrivateKeySigner = ALICE_PK.parse().expect("Failed to parse pk");
-            let wallet = EthereumWallet::from(signer);
-
-            let chain_id = provider
-                .get_chain_id()
-                .await
-                .expect("Failed to get chain id");
-
-            // Get accout nonce
             let nonce = provider
                 .get_transaction_count(account_alice)
                 .await
                 .expect("Failed to get nonce");
 
-            let tx = sword_contract
-                .mint(account_alice, Uint::from(2))
-                .into_transaction_request();
-
-            // Use the contract to make async calls.
-            let envelope = tx
-                .with_nonce(nonce)
-                .with_from(account_alice)
-                .with_max_fee_per_gas(MAX_TESTNET_GAS_PRICE)
-                .with_gas_limit(40_000)
-                .with_chain_id(chain_id)
-                .with_max_priority_fee_per_gas(1_000_000_000)
-                .build(&wallet)
+            let tx_hash = sword_contract
+                .mint(account_alice, Uint::from(3))
+                .nonce(nonce + 1)
+                .send()
                 .await
-                .expect("Failed to build tx");
-
-            let pending_tx = provider
-                .send_tx_envelope(envelope)
+                .expect("Failed to mint sword")
+                .watch()
                 .await
-                .expect("Failed to send tx");
+                .expect("Failed to watch");
 
-            let receipt = pending_tx
-                .get_receipt()
-                .await
-                .expect("Failed to get receipt");
-            println!(
-                "Minted sword with tx hash: {} and block number: {}",
-                receipt.transaction_hash,
-                receipt.block_number.expect("Failed to get block number")
-            );
+            println!("Minted sword with tx hash: {}", tx_hash);
 
             // Transfer sword from Alice to Bob
 
             let account_bob = Address::from_str(BOB_ADDRESS).unwrap();
 
-            let tx = sword_contract
-                .transferFrom(account_alice, account_bob, Uint::from(2))
-                .into_transaction_request();
-
-            let nonce = provider
-                .get_transaction_count(account_alice)
+            let tx_hash = sword_contract
+                .transferFrom(account_alice, account_bob, Uint::from(3))
+                .send()
                 .await
-                .expect("Failed to get nonce");
-
-            let envelope = tx
-                .with_nonce(nonce)
-                .with_from(account_alice)
-                .with_max_fee_per_gas(MAX_TESTNET_GAS_PRICE)
-                .with_gas_limit(40_000)
-                .with_chain_id(chain_id)
-                .with_max_priority_fee_per_gas(1_000_000_000)
-                .build(&wallet)
+                .expect("Failed to transfer sword")
+                .watch()
                 .await
-                .expect("Failed to build tx");
+                .expect("Failed to watch");
 
-            let receipt = provider
-                .send_tx_envelope(envelope)
-                .await
-                .expect("Failed to send tx")
-                .get_receipt()
-                .await
-                .expect("Failed to get receipt");
-
-            println!(
-                "Transferred sword with tx hash: {}",
-                receipt.transaction_hash
-            );
+            println!("Transferred sword with tx hash: {}", tx_hash);
         }
     });
 }
 
-pub fn listen_to_transfers(
+pub fn listen_to_gold_transfers(
     runtime: Res<TokioTasksRuntime>,
-    provider: Res<EthProviderResource>,
-    sword_contract_res: Res<SwordContractResource>,
+    gold_contract_res: Res<GoldContractResource>,
 ) {
-    let sword_contract = sword_contract_res.0.clone();
-    let provider = provider.0.clone();
+    let gold_contract = gold_contract_res.0.clone();
     runtime.spawn_background_task(|_ctx| {
         // Move everything into the async block, ensuring all variables are owned.
         async move {
-            let transfer_filter = Filter::new()
-                .address(*sword_contract.address())
-                .events([SwordContract::Transfer::SIGNATURE]);
+            let gold_filter = gold_contract
+                .Transfer_filter()
+                .watch()
+                .await
+                .expect("Failed to watch");
 
-            let Ok(logs) = provider.get_logs(&transfer_filter).await else {
-                todo!()
-            };
+            gold_filter
+                .into_stream()
+                .take(1)
+                .for_each(|log| {
+                    match log {
+                        Ok((_event, log)) => {
+                            println!("Received Gold Transfer: {log:?}");
+                        }
+                        Err(e) => {
+                            println!("Log event error: {e:?}");
+                        }
+                    };
+                })
+                .await;
+        }
+    });
+}
 
-            if let Some(last_event) = logs.last() {
-                if let Ok(transfer) = last_event.log_decode::<SwordContract::Transfer>() {
-                    println!("Transfer event: {:?}", transfer.address());
-                }
+pub fn listen_to_sword_transfers(
+    runtime: Res<TokioTasksRuntime>,
+    sword_contract_res: Res<SwordContractResource>,
+) {
+    let sword_contract = sword_contract_res.0.clone();
+    runtime.spawn_background_task(|_ctx| {
+        // Move everything into the async block, ensuring all variables are owned.
+        async move {
+            let transfer_filter = sword_contract
+                .Transfer_filter()
+                .watch()
+                .await
+                .expect("Failed to watch");
+
+            transfer_filter
+                .into_stream()
+                .take(1)
+                .for_each(|log| {
+                    match log {
+                        Ok((_event, log)) => {
+                            println!("Received Sword Transfer: {log:?}");
+                        }
+                        Err(e) => {
+                            println!("Log event error: {e:?}");
+                        }
+                    };
+                })
+                .await;
+        }
+    });
+}
+
+pub fn try_gold_transfer(
+    runtime: Res<TokioTasksRuntime>,
+    gold_contract_res: Res<GoldContractResource>,
+) {
+    let gold_contract = gold_contract_res.0.clone();
+    runtime.spawn_background_task(|_ctx| {
+        // Move everything into the async block, ensuring all variables are owned.
+        async move {
+            let account_bob = Address::from_str(BOB_ADDRESS).unwrap();
+
+            let tx = gold_contract
+                .transfer(account_bob, Uint::from(1))
+                .send()
+                .await
+                .expect("Failed to transfer gold")
+                .watch()
+                .await
+                .expect("Failed to watch");
+
+            println!("Transferred gold with tx hash: {}", tx);
+
+            let accounts: Vec<Address> = vec![
+                Address::from_str(ALICE_ADDRESS).unwrap(),
+                Address::from_str(BOB_ADDRESS).unwrap(),
+            ];
+
+            for account in accounts {
+                // Use the contract to make async calls.
+                let res = gold_contract.balanceOf(account).call().await.unwrap();
+                let balance = res._0;
+                println!("Balance of {}: {}", account, balance);
             }
         }
     });
@@ -220,9 +257,15 @@ impl Plugin for DpPlugin {
 
         let rpc_url = "http://127.0.0.1:8545";
 
+        let signer: PrivateKeySigner = ALICE_PK.parse().expect("should parse private key");
+        let wallet = EthereumWallet::from(signer);
+
         // Create the provider and wrap it in Arc to share ownership across async blocks.
         let provider = Arc::new(
-            ProviderBuilder::new().on_http(rpc_url.parse().expect("Failed to parse rpc url")),
+            ProviderBuilder::new()
+                .with_recommended_fillers()
+                .wallet(wallet)
+                .on_http(rpc_url.parse().expect("Failed to parse rpc url")),
         );
 
         // Create the gold_contract and wrap it in Arc to allow thread-safe access.
@@ -233,13 +276,13 @@ impl Plugin for DpPlugin {
 
         // Create the sword_contract and wrap it in Arc to allow thread-safe access.
         let sword_contract = SwordContract::new(
-            address!("Cf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"),
+            address!("e7f1725E7734CE288F8367e1Bb143E90bb3F0512"),
             provider.clone(), // Clone the Arc to share ownership of the provider.
         );
 
         app.add_plugins(bevy_tokio_tasks::TokioTasksPlugin::default())
             .insert_resource(GameMap(game_map))
-            .insert_resource(EthProviderResource(Arc::new(provider)))
+            .insert_resource(EthProviderResource(provider))
             .insert_resource(GoldContractResource(Arc::new(gold_contract)))
             .insert_resource(SwordContractResource(Arc::new(sword_contract)))
             .add_event::<GainScoreEvent>()
@@ -247,8 +290,10 @@ impl Plugin for DpPlugin {
                 Startup,
                 (
                     display_balances,
-                    listen_to_transfers,
-                    try_mint_sword,
+                    listen_to_gold_transfers,
+                    // listen_to_sword_transfers,
+                    try_gold_transfer,
+                    // try_mint_sword,
                     setup,
                     ui::systems::setup,
                     map::systems::setup,
